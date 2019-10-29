becf1678617042b07e30b8caa178fb3ffcaf9ee1
weyheyhey <iweyheyhey@gmail.com>
2019-10-29 15:08:21 +0300

    chore(lint): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  root: true,
+  extends: ["@wowvendor/eslint-config-base"],
+};
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [2, 'always', 100],
-    'type-empty': [2, 'never'],
-    'type-case': [2, 'always', 'lower-case'],
-    'scope-case': [2, 'always', 'kebab-case'],
-    'type-enum': [
+    "header-max-length": [2, "always", 100],
+    "type-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", "kebab-case"],
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat', // A new feature
-        'fix', // A bug fix
-        'wip', // Work in progress
-        'chore', // Build process or auxiliary tool changes
-        'refactor', // A code change that neither fixes a bug or adds a feature
-        'style', // Markup, white-space, formatting, missing semi-colons...
-        'test', // Adding missing tests
-        'perf', // A code change that improves performance,
-        'docs', // Documentation only changes
-        'ci', // CI related changes
-        'revert', // Revert to a commit
+        "feat", // A new feature
+        "fix", // A bug fix
+        "wip", // Work in progress
+        "chore", // Build process or auxiliary tool changes
+        "refactor", // A code change that neither fixes a bug or adds a feature
+        "style", // Markup, white-space, formatting, missing semi-colons...
+        "test", // Adding missing tests
+        "perf", // A code change that improves performance,
+        "docs", // Documentation only changes
+        "ci", // CI related changes
+        "revert", // Revert to a commit
       ],
     ],
   },
